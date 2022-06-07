90f8ef65be0d13660d8b4595dccce03a5310b80b
TrustworthyBlake <andreasblakli@gmail.com>
2022-06-07 02:27:42 +0200

    Implemented footer.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,11 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='project__footer section__padding' id="footer">
+      <div className='project__footer-container'>
+        <div />
+      </div>
+    </div>
   )
 }
 
