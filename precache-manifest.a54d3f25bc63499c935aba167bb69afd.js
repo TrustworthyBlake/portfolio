490da19942cd01ceea47c069fdeed6468040db74
TrustworthyBlake <andreasblakli@gmail.com>
2022-06-07 03:27:15 +0200

    Updates

---
+++ precache-manifest.a54d3f25bc63499c935aba167bb69afd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b22093df3505df8a742",
+    "url": "/portfolio/static/css/main.f8d30923.chunk.css"
+  },
+  {
+    "revision": "1b22093df3505df8a742",
+    "url": "/portfolio/static/js/main.1b22093d.chunk.js"
+  },
+  {
+    "revision": "919a0dc48b36787fb2f7",
+    "url": "/portfolio/static/js/1.919a0dc4.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4955974fd5fa64f57e7b5b74f9dd2542",
+    "url": "/portfolio/index.html"
+  }
+];
